23b97d2a8798e3239ec9ca39a3fabe8f0f9c4469
davelens <github@davelens.be>
2015-12-23 12:12:11 +0100

    update to newer rspec syntax

--- spec/click_spec.rb
+++ spec/click_spec.rb
@@ -1,8 +1,8 @@
 require 'trade_tracker'
 
 describe TradeTracker::Click do
-  before(:each) do
-    @tt = TradeTracker::Click.new(
+  let(:instance) do
+    TradeTracker::Click.new(
       campaignID: '14986',
       material_id: '0',
       affiliate_id: '18897',
@@ -11,65 +11,81 @@ describe TradeTracker::Click do
     )
   end
 
-  describe 'parameters' do
-    it 'should filter campaign_id' do
-      @tt.campaign_id = 1234
-      @tt.campaign_id.should == '1234'
-
-      @tt.campaign_id = 0
-      @tt.campaign_id.should == '0'
+  describe '#campaign_id' do
+    it 'result' do
+      instance.campaign_id = 1234
+      expect(instance.campaign_id).to eq '1234'
     end
 
-    it 'should filter affiliate_id' do
-      @tt.affiliate_id = 1234
-      @tt.affiliate_id.should == '1234'
-
-      @tt.affiliate_id = 0
-      @tt.affiliate_id.should == '0'
-    end
-
-    it 'should filter material_id' do
-      @tt.material_id = 20
-      @tt.material_id.should == '20'
-
-      @tt.material_id = 0
-      @tt.material_id.should == '0'
-    end
-
-    it 'should filter an empty redirect_url' do
-      @tt.redirect_url = 'http://www.google.be'
-      @tt.redirect_url.should == 'http%3A%2F%2Fwww.google.be'
-
-      @tt.redirect_url = nil
-      @tt.redirect_url.should == ''
-    end
-
-    it 'should filter an empty reference' do
-      @tt.reference = 'X254'
-      @tt.reference.should == 'X254'
-
-      @tt.reference = nil
-      @tt.reference.should == ''
+    it 'blank' do
+      instance.campaign_id = 0
+      expect(instance.campaign_id).to eq '0'
     end
   end
 
-  it 'should return the correct cookie name' do
-    @tt.campaign_id = 'foo'
-    @tt.cookie_name.should == 'TT2_foo'
-  end
+  describe '#affiliate_id' do
+    it 'result' do
+      instance.affiliate_id = 1234
+      expect(instance.affiliate_id).to eq '1234'
+    end
 
-  it 'should return the correct cookie value' do
-    @tt.cookie_value.should include '0::18897::::c83b23df3e4be73b2aa751dbd2fc248e::'
-  end
-
-  describe 'trackback_url' do
-    it 'should return the correct tradetracker trackback URL' do
-      @tt.trackback_url.should == 'http://tc.tradetracker.net/?c=14986&m=0&a=18897&r=&u='
+    it 'blank' do
+      instance.affiliate_id = 0
+      expect(instance.affiliate_id).to eq '0'
     end
   end
 
-  it 'should respond to some methods' do
-    @tt.should respond_to(
+  describe '#material_id' do
+    it 'result' do
+      instance.material_id = 20
+      expect(instance.material_id).to eq '20'
+    end
+
+    it 'blank' do
+      instance.material_id = 0
+      expect(instance.material_id).to eq '0'
+    end
+  end
+
+  describe '#redirect_url' do
+    it 'filled' do
+      instance.redirect_url = 'http://www.google.be'
+      expect(instance.redirect_url).to eq 'http%3A%2F%2Fwww.google.be'
+    end
+
+    it 'blank' do
+      instance.redirect_url = nil
+      expect(instance.redirect_url).to eq ''
+    end
+  end
+
+  describe '#reference' do
+    it 'filled' do
+      instance.reference = 'X254'
+      expect(instance.reference).to eq 'X254'
+    end
+
+    it 'blank' do
+      instance.reference = nil
+      expect(instance.reference).to eq ''
+    end
+  end
+
+  it '#cookie_name' do
+    instance.campaign_id = 'foo'
+    expect(instance.cookie_name).to eq 'TT2_foo'
+  end
+
+  it '#cookie_value' do
+    expect(instance.cookie_value).to include '0::18897::::c83b23df3e4be73b2aa751dbd2fc248e::'
+  end
+
+  it '#trackback_url' do
+    expect(instance.trackback_url).to eq 'http://tc.tradetracker.net/?c=14986&m=0&a=18897&r=&u='
+  end
+
+  it '#respond_to?' do
+    expect(instance).to respond_to(
       :campaign_id=, :campaign_id, :material_id=, :material_id, :affiliate_id=,
       :affiliate_id, :reference=, :reference, :redirect_url=, :redirect_url,
       :cookie_name, :cookie_value, :trackback_url
--- spec/conversion_spec.rb
+++ spec/conversion_spec.rb
@@ -1,8 +1,8 @@
 require 'trade_tracker'
 
 describe TradeTracker::Conversion do
-  before(:each) do
-    @tt = TradeTracker::Conversion.new(
+  let(:instance) do
+    TradeTracker::Conversion.new(
       campaign_id: '14986',
       product_id: '22367',
       conversion_type: 'sales',
@@ -10,65 +10,76 @@ describe TradeTracker::Conversion do
     )
   end
 
-  describe 'parameters' do
-    it 'should filter campaign_id' do
-      @tt.campaign_id = 1234
-      @tt.campaign_id.should == '1234'
-
-      @tt.campaign_id = 0
-      @tt.campaign_id.should == '0'
+  describe '#campaign_id' do
+    it 'result' do
+      instance.campaign_id = 1234
+      expect(instance.campaign_id).to eq '1234'
     end
 
-    it 'should filter product_id' do
-      @tt.product_id = 20
-      @tt.product_id.should == '20'
-
-      @tt.product_id = 0
-      @tt.product_id.should == '0'
-    end
-
-    it 'should filter conversion_type' do
-      @tt.conversion_type = 1234
-      @tt.conversion_type.should == '1234'
-
-      @tt.conversion_type = 0
-      @tt.conversion_type.should == '0'
-    end
-
-    it 'should filter https state' do
-      @tt.use_https.should == true
-
-      @tt.use_https = '0'
-      @tt.use_https.should == false
-    end
-
-    it 'should filter transaction amount' do
-      @tt.transaction_amount = '1234'
-      @tt.transaction_amount.should == 1234.0
-
-      @tt.transaction_amount = '405.76'
-      @tt.transaction_amount.should == 405.76
+    it 'blank' do
+      instance.campaign_id = 0
+      expect(instance.campaign_id).to eq '0'
     end
   end
 
-  describe 'trackback_url' do
-    it 'should raise an exception when no campaign ID is set' do
-      @tt.campaign_id = nil
-      expect { @tt.trackback_url }.to raise_error('Wrong or missing parameters')
+  describe '#product_id' do
+    it 'result' do
+      instance.product_id = 20
+      expect(instance.product_id).to eq '20'
     end
 
-    it 'should raise an exception when no product ID is set' do
-      @tt.product_id = nil
-      expect { @tt.trackback_url }.to raise_error('Wrong or missing parameters')
-    end
-
-    it 'should return the correct trackback URL' do
-      @tt.trackback_url.should == 'https://ts.tradetracker.net/?cid=14986&pid=22367&data=&type=&tid=&tam=0.0&qty=&eml=&descrMerchant=&descrAffiliate='
+    it 'blank' do
+      instance.product_id = 0
+      expect(instance.product_id).to eq '0'
     end
   end
 
-  it 'should respond to some methods' do
-    @tt.should respond_to(
+  describe '#conversion_type' do
+    it 'result' do
+      instance.conversion_type = 1234
+      expect(instance.conversion_type).to eq '1234'
+    end
+
+    it 'blank' do
+      instance.conversion_type = 0
+      expect(instance.conversion_type).to eq '0'
+    end
+  end
+
+  describe '#use_https' do
+    it 'true' do
+      expect(instance.use_https).to be true
+    end
+
+    it 'false' do
+      instance.use_https = '0'
+      expect(instance.use_https).to be false
+    end
+  end
+
+  it '#transaction_amount' do
+    instance.transaction_amount = '1234'
+    expect(instance.transaction_amount).to eq 1234.0
+  end
+
+  describe '#trackback_url' do
+    it 'no campaign ID' do
+      instance.campaign_id = nil
+      expect { instance.trackback_url }.to raise_error('Wrong or missing parameters')
+    end
+
+    it 'no product ID' do
+      instance.product_id = nil
+      expect { instance.trackback_url }.to raise_error('Wrong or missing parameters')
+    end
+
+    it 'result' do
+      expect(instance.trackback_url).to eq 'https://ts.tradetracker.net/?cid=14986&pid=22367&data=&type=&tid=&tam=0.0&qty=&eml=&descrMerchant=&descrAffiliate='
+    end
+  end
+
+  it '#respond_to?' do
+    expect(instance).to respond_to(
       :campaign_id=, :campaign_id, :product_id=, :product_id, :conversion_type=,
       :conversion_type, :use_https=, :use_https, :tracking_data=,
       :tracking_data, :transaction_id=, :transaction_id, :transaction_amount=,
