a744da9dc5566b7963de4ef141836c1da44774fe
davelens <github@davelens.be>
2018-01-12 10:29:28 +0100

    update click_spec to use #subject instead of #instance

--- spec/click_spec.rb
+++ spec/click_spec.rb
@@ -1,7 +1,7 @@
 require 'trade_tracker'
 
 describe TradeTracker::Click do
-  let(:instance) do
+  subject do
     TradeTracker::Click.new(
       campaignID: '14986',
       material_id: '0',
@@ -13,79 +13,79 @@ describe TradeTracker::Click do
 
   describe '#campaign_id' do
     it 'result' do
-      instance.campaign_id = 1234
-      expect(instance.campaign_id).to eq '1234'
+      subject.campaign_id = 1234
+      expect(subject.campaign_id).to eq '1234'
     end
 
     it 'blank' do
-      instance.campaign_id = 0
-      expect(instance.campaign_id).to eq '0'
+      subject.campaign_id = 0
+      expect(subject.campaign_id).to eq '0'
     end
   end
 
   describe '#affiliate_id' do
     it 'result' do
-      instance.affiliate_id = 1234
-      expect(instance.affiliate_id).to eq '1234'
+      subject.affiliate_id = 1234
+      expect(subject.affiliate_id).to eq '1234'
     end
 
     it 'blank' do
-      instance.affiliate_id = 0
-      expect(instance.affiliate_id).to eq '0'
+      subject.affiliate_id = 0
+      expect(subject.affiliate_id).to eq '0'
     end
   end
 
   describe '#material_id' do
     it 'result' do
-      instance.material_id = 20
-      expect(instance.material_id).to eq '20'
+      subject.material_id = 20
+      expect(subject.material_id).to eq '20'
     end
 
     it 'blank' do
-      instance.material_id = 0
-      expect(instance.material_id).to eq '0'
+      subject.material_id = 0
+      expect(subject.material_id).to eq '0'
     end
   end
 
   describe '#redirect_url' do
     it 'filled' do
-      instance.redirect_url = 'http://www.google.be'
-      expect(instance.redirect_url).to eq 'http%3A%2F%2Fwww.google.be'
+      subject.redirect_url = 'http://www.google.be'
+      expect(subject.redirect_url).to eq 'http%3A%2F%2Fwww.google.be'
     end
 
     it 'blank' do
-      instance.redirect_url = nil
-      expect(instance.redirect_url).to eq ''
+      subject.redirect_url = nil
+      expect(subject.redirect_url).to eq ''
     end
   end
 
   describe '#reference' do
     it 'filled' do
-      instance.reference = 'X254'
-      expect(instance.reference).to eq 'X254'
+      subject.reference = 'X254'
+      expect(subject.reference).to eq 'X254'
     end
 
     it 'blank' do
-      instance.reference = nil
-      expect(instance.reference).to eq ''
+      subject.reference = nil
+      expect(subject.reference).to eq ''
     end
   end
 
   it '#cookie_name' do
-    instance.campaign_id = 'foo'
-    expect(instance.cookie_name).to eq 'TT2_foo'
+    subject.campaign_id = 'foo'
+    expect(subject.cookie_name).to eq 'TT2_foo'
   end
 
   it '#cookie_value' do
-    expect(instance.cookie_value).to include '0::18897::::c83b23df3e4be73b2aa751dbd2fc248e::'
+    expect(subject.cookie_value).to include '0::18897::::c83b23df3e4be73b2aa751dbd2fc248e::'
   end
 
   it '#trackback_url' do
-    expect(instance.trackback_url).to eq 'http://tc.tradetracker.net/?c=14986&m=0&a=18897&r=&u='
+    expect(subject.trackback_url).to eq 'http://tc.tradetracker.net/?c=14986&m=0&a=18897&r=&u='
   end
 
   it '#respond_to?' do
-    expect(instance).to respond_to(
+    expect(subject).to respond_to(
       :campaign_id=, :campaign_id, :material_id=, :material_id, :affiliate_id=,
       :affiliate_id, :reference=, :reference, :redirect_url=, :redirect_url,
       :cookie_name, :cookie_value, :trackback_url
